51a90a559e5876082e8da38873723b55ca73af39
Gabriel Sponda Freitas Bettarello <sponda.gabriel@aluno.ufabc.edu.br>
2023-12-08 14:14:13 -0300

    Iniciando projeto do blog pessoal.

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,69 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_postagens")
+public class Postagem {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 100)
+	@NotBlank(message = "O título é obrigatório!")
+	@Size(min =5, max = 100, message = "O título deve conter no mínimo 5 caracteres e no máximo 100!")
+	private String titulo;
+	
+	@Column(length = 1000)
+	@NotBlank(message = "O texto é obrigatório!")
+	@Size(min =10, max = 1000, message = "O texto deve conter no mínimo 10 caracteres e no máximo 1000!")
+	private String texto;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+
+}
